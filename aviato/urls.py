4a04e1ad29eadc71d85584db58ec8cdc3f8925dc
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-26 20:34:18 +0500

    add blog page url

--- aviato/urls.py
+++ aviato/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, BlogPageView
 
 app_name = 'aviato'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('blog-info/', BlogPageView.as_view(), name='blog'),
 ]
