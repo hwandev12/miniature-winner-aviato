17ef3d0328675e56ac607a6dd28b0b3034eca067
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-26 13:47:37 +0500

    add login view url

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import (
+    LoginView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('aviato.urls'))
+    path('', include('aviato.urls')),
+    path('login/', LoginView.as_view(), name='login')
 ]
