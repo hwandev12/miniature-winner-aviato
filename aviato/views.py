fb3df693052ae2ef25cf2cbd9a5005f33d0808ac
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-26 20:34:31 +0500

    create blog page view

--- aviato/views.py
+++ aviato/views.py
@@ -5,3 +5,7 @@ from django.views.generic import (
 
 class HomeView(TemplateView):
     template_name = 'pages/home.html'
+
+
+class BlogPageView(TemplateView):
+    template_name = 'pages/blog_page.html'
