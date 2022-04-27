8c39d3093852da5d5fa538df92238d2f7b278f69
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-27 09:43:12 +0500

    add about url

--- aviato/urls.py
+++ aviato/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import HomeView, BlogPageView
-
+from .views import (
+    HomeView,
+    BlogPageView,
+    AboutPageView
+)
 app_name = 'aviato'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('blog-info/', BlogPageView.as_view(), name='blog'),
+    path('about/', AboutPageView.as_view(), name='about')
 ]
