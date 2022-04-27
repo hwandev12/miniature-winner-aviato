b1166174289bc21ddd614fa12fe36265f42bcc1a
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-27 10:39:32 +0500

    soleve problems

--- aviato/models.py
+++ aviato/models.py
@@ -8,10 +8,14 @@ class User(AbstractUser):
 
 class MainHomePageImages(models.Model):
     class Meta:
-        verbose_name = 'Home Images'
+        verbose_name = 'Home'
         verbose_name_plural = 'Home Page Images'
 
+    text = models.CharField(max_length=60)
     carousel_img = models.ImageField()
     product_category = models.ImageField()
     trendy_products = models.ImageField()
     instagram = models.ImageField()
+
+    def __str__(self):
+        return self.text
