00470cd6af0afaf40f2fc9e82b1a60e16c664cc0
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-27 10:16:15 +0500

    add media images base field

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth.views import (
     LoginView
 )
@@ -9,3 +11,7 @@ urlpatterns = [
     path('', include('aviato.urls')),
     path('login/', LoginView.as_view(), name='login')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
