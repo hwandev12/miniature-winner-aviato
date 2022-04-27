415aafdea94360fa5e1002c7c60cacee64e3846d
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-27 10:12:25 +0500

    create home page image models

--- aviato/models.py
+++ aviato/models.py
@@ -4,3 +4,14 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+
+class MainHomePageImages(models.Model):
+    class Meta:
+        verbose_name = 'Home Images'
+        verbose_name_plural = 'Home Page Images'
+
+    carousel_img = models.ImageField()
+    product_category = models.ImageField()
+    trendy_products = models.ImageField()
+    instagram = models.ImageField()
