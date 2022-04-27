ee66343916865f5ed79264988ac141fb73d4e250
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-27 09:48:02 +0500

    add dashboard url

--- aviato/urls.py
+++ aviato/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from .views import (
     HomeView,
     BlogPageView,
-    AboutPageView
+    AboutPageView,
+    DashboardPageView
 )
 app_name = 'aviato'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('blog-info/', BlogPageView.as_view(), name='blog'),
-    path('about/', AboutPageView.as_view(), name='about')
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('dashboard/', DashboardPageView.as_view(), name='dashboard')
 ]
