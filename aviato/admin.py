7566b9ece85d34e961d4a38e6f151c9dcaa2da2f
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-30 05:36:53 +0500

    add product category to admin

--- aviato/admin.py
+++ aviato/admin.py
@@ -4,11 +4,19 @@ from .models import (
     Header,
     UserProfile,
     Agent,
+    ProductCategory,
     User
 )
 
+# import user
 admin.site.register(User)
+# import base header and carousel
 admin.site.register(CarouselImages)
 admin.site.register(Header)
+# import product category
+admin.site.register(ProductCategory)
+
+
+# import basic agent and userprofiles
 admin.site.register(UserProfile)
 admin.site.register(Agent)
